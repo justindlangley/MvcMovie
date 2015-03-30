9e83e830d3a966ce3a2428a9d55318671b307d74
justindlangley <justindlangley@gmail.com>
2015-03-30 14:23:35 -0400

    Added test

---
+++ MCVMovieTest/Class1.cs
@@ -0,0 +1,28 @@
+﻿using MvcMovie.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Xunit;
+
+namespace MCVMovieTest
+{
+    public class Class1
+    {
+        [Fact]
+
+        public void SomeTest()
+        {
+            var controller = new HomeController();
+            var someView = controller.About();
+            Assert.NotNull(someView);
+            
+            var theView = (ViewResult)someView;
+            Assert.NotNull(theView);
+            
+        }
+
+    }
+}
