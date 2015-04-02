c9e62c8602d0b2c9050945a4a9e0d2ca5b292adc
justindlangley <justindlangley@gmail.com>
2015-04-02 10:42:17 -0400

    Added rating
    to Models>Movie.cs

--- MvcMovie/Migrations/Configuration.cs
+++ MvcMovie/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace MvcMovie.Migrations
 {
+    using MvcMovie.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -28,15 +29,40 @@ namespace MvcMovie.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
-            context.Movies.AddOrUpdate( i = i.Title,
-                new Movie 
+            context.Movies.AddOrUpdate(i => i.Title,
+                new Movie
                 {
-                   Title = 
-                   ReleaseDate = 
-                   Genre = 
-                   Price =
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-1-11"),
+                    Genre = "Romantic Comedy",
+                    Price = 8.99M
 
-                })
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Price = 8.99M
+
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Price = 9.99M
+
+                },
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Price = 3.99M
+
+                }
+                );
         }
     }
 }
