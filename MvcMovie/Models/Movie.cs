6ebcfbd0365707bc3f6879a2d8f1fd6a9e09fbcf
justindlangley <justindlangley@gmail.com>
2015-04-03 10:02:58 -0400

    Added more attributes
    ..

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -10,6 +10,8 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
         
 
@@ -31,6 +33,8 @@ namespace MvcMovie.Models
         [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(5)]
         public string Rating { get; set; }
 
     }
