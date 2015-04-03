9333ef73f47b9a5bf18b0716181d6a3c26f06725
justindlangley <justindlangley@gmail.com>
2015-04-03 09:56:19 -0400

    Added validations
    to genre

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -21,8 +21,16 @@ namespace MvcMovie.Models
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0: yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
         public string Rating { get; set; }
 
     }
